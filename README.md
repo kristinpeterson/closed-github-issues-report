9f2c35a7c2ac6728d00eecc641ea678a71432a81
kristinpeterson <kristinpeterson@me.com>
2016-05-07 16:53:42 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,48 @@
+## Closed GitHub Issues Report
+
+The purpose of this script is to prepare a report of all GitHub issues closed for a set of repositories over a given timeframe. The resulting report is segmented by week.
+
+Example Report Output: https://gist.github.com/kristinpeterson/a97543e792ed82d9ba1a7adc366e1eba
+
+### How to specify which repositories to include in the report
+
+Create a file called `repos.json` and store it in the root of the project, see [`repos.sample`](repos.sample) for an example. Each library is represented as a JSON object with the following structure:
+
+```
+{ 
+  "name": "repo-name",
+  "owner": "repo-owner"
+}
+```
+
+### Prerequisites
+
+A GitHub [access token](https://help.github.com/articles/creating-an-access-token-for-command-line-use/) from an account that has `PULL` access to the target repositories must be created.
+
+1. Go to: https://github.com/settings/tokens
+2. Generate an token with access to the following scopes: `repo` and `public_repo`
+3. Make a copy of [`settings.sample`](settings.sample) and rename to `settings.txt`
+4. Assign the access token created in step 2 to `github_access_token` in `settings.txt`, Example:
+
+```
+github_access_token=[GITHUB_ACCESS_TOKEN_HERE]
+```
+
+### Dependencies
+
+See [`requirements.txt`](/requirements.txt) for dependency list. I suggest using [`virtualenv`](https://virtualenv.readthedocs.org/en/latest/) to manage your dependencies.
+
+### Usage
+
+1. Confirm that the repos listed in `repos.json` are accurate
+2. Run the script from the root directory of the project: 
+
+```
+python main.py <report_weeks> <report_start_date> <report_end_date>
+```
+
+Command line arguments:
+
+* <report_weeks> (required) number of weeks to include in the report
+* <report_start_date> (required) start date of report formatted as YYYY-MM-DD (Monday)
+* <report_end_date> (required) end date of report formatted as YYYY-MM-DD (Sunday)
---
+++ issues_closed/issues_closed.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+import requests
+import json
+import dateutil.parser
+import os
+import time
+from datetime import date, timedelta
+
+DATA_DIRECTORY = "data/"
+GITHUB_BASE_URL = "https://api.github.com/repos/"
+
+def run(settings, report_weeks, report_start_date, report_end_date):
+    """
+    Generates a report of all closed github issues for att-projects repos.
+
+    Keyword Arguments:
+    settings -- an array of settings variables, derived from `/settings.txt`
+    report_weeks -- the number of weeks worth of posts to include in the report
+    report_end_date -- datetime.date object representing the end date of the report
+    """
+    headers = { 'Authorization': 'token ' + settings["github_access_token"] }
+    get_issue_data(headers, report_start_date)
+    prepare_issue_report(report_weeks, report_end_date)
+    cleanup_data_dir()
+
+def get_issue_data(headers, start_date):
+    """
+    Gets repo issue data and saves to DATA_DIRECTORY, one file per repository.
+    Note that the data files saved are temporary and will be deleted.
+
+    Keyword Arguments:
+    headers -- headers used when making request to GitHub
+    start_date -- all issues closed since this date will be retrieved
+    """
+    print "iterating over repos and saving closed issue data to data files..."
+    repos = get_repos()
+    
+    for repo in repos:
+        issues_url = GITHUB_BASE_URL + repo['owner'] + "/" + repo['name'] + "/issues?state=closed&per_page=100&since=" + start_date
+        github_response = requests.get(issues_url, headers=headers)
+        
+        json_data = github_response.json()
+        continue_paging = True
+
+        while continue_paging:
+            try:
+                links = parse_link_header(github_response.headers['link'])
+                github_response = requests.get(links['next'], headers=headers)
+                json_data = json_data + github_response.json()
+            except Exception:
+                continue_paging = False
+
+        with open(DATA_DIRECTORY + repo['owner'] + "_" + repo['name'], 'w') as outfile:
+            json.dump(json_data, outfile)
+
+def get_repos():
+    """
+    Loads repository data from repos.json into JSON object
+    """
+    try:
+        with open('repos.json') as data_file:    
+            repos = json.load(data_file)
+        return repos
+    except:
+        print "Error loading repos.json"
+        sys.exit()
+
+def parse_link_header(link):
+    """
+    Parses the link header in github request
+
+    See: https://developer.github.com/v3/#link-header
+    """
+    links = {}
+    linkHeaders = link.split(", ")
+    for linkHeader in linkHeaders:
+        (url, rel) = linkHeader.split("; ")
+        url = url[1:-1]
+        rel = rel[5:-1]
+        links[rel] = url
+    return links
+
+def prepare_issue_report(report_weeks, report_end_date):
+    """
+    Prepares github issue data into a single file report 
+    which is timestamped and saved to the output directory
+    """
+    print "preparing report..."
+    report = open("output/report-" + time.strftime("%Y-%m-%dT%H:%M:%SZ") + ".txt", 'w')
+    i = 0
+    while i < report_weeks:
+        week_end_date = report_end_date - timedelta(days = i * 7)
+        week_start_date = week_end_date - timedelta(days = 6)
+        report_header = "Issues completed from " + week_start_date.strftime("%m/%d/%Y") + " to " + week_end_date.strftime("%m/%d/%Y")
+        report.write("==============================================\n")
+        report.write(report_header)
+        report.write("\n==============================================\n\n")
+
+        for data_file in os.listdir("data"):
+            updated = False
+
+            with open("data/" + data_file) as df:    
+                data = json.load(df)
+
+            for pr in data:
+                pr_closed_at = dateutil.parser.parse(pr['closed_at']).date()
+                if week_end_date >= pr_closed_at > week_end_date - timedelta(days = 7):
+                    if updated is False:
+                        updated = True
+                        report.write("--------------------------------------\n" + data_file.replace("_", "/") + ":\n--------------------------------------\n\n")
+                    line = ("* " + pr['title'] + "\n" + pr['html_url'] + "\n").encode('ascii', 'ignore').decode('ascii')
+                    report.write(line)
+            if updated is True: 
+                report.write("\n")
+
+        i = i + 1
+
+def cleanup_data_dir():
+    """
+    Deletes all files in the data directory
+    """
+    print "cleaning up data directory..."
+    file_list = [ f for f in os.listdir(DATA_DIRECTORY) ]
+    for f in file_list:
+        os.remove(DATA_DIRECTORY + f)
