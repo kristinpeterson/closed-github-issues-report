4fe642470e829647a7ac9ef61b07e526dafb8d63
kristinpeterson <kristinpeterson@me.com>
2016-05-08 19:38:53 -0700

    Refactoring/cleanup

--- issues_closed/issues_closed.py
+++ issues_closed/issues_closed.py
@@ -7,14 +7,9 @@ import os
 import time
 from datetime import date, timedelta
 
-SETTINGS = get_settings()
 DATA_DIRECTORY = "data"
 OUTPUT_DIRECTORY = "output"
 GITHUB_BASE_URL = "https://api.github.com/repos/"
-GITHUB_ACCESS_TOKEN = SETTINGS["github_access_token"]
-REPORT_WEEKS = get_report_weeks()
-REPORT_START_DATE = get_report_start_date_as_ISO_string()
-REPORT_END_DATE = get_report_end_date_as_datetime_obj()
 
 def run():
     """
@@ -41,40 +36,41 @@ def store_issue_data():
     """
     print "iterating over repos and saving closed issue data to data files..."
     repos = get_repos()
-    headers = { 'Authorization': 'token ' + GITHUB_ACCESS_TOKEN }
-    
     for repo in repos:
-        issues_url = GITHUB_BASE_URL + repo['owner'] + "/" + repo['name'] + "/issues?state=closed&per_page=100&since=" + REPORT_START_DATE
-        github_response = requests.get(issues_url, headers=headers)
-        
-        json_data = github_response.json()
-        continue_paging = True
-
-        while continue_paging:
-            try:
-                links = parse_link_header(github_response.headers['link'])
-                github_response = requests.get(links['next'], headers=headers)
-                json_data = json_data + github_response.json()
-            except Exception:
-                continue_paging = False
+        issue_data = get_issue_data(repo)
 
         with open(DATA_DIRECTORY + "/" + repo['owner'] + "_" + repo['name'], 'w') as outfile:
-            json.dump(json_data, outfile)
+            json.dump(issue_data, outfile)
 
 def get_issue_data(repo):
-
+    headers = { 'Authorization': 'token ' + get_settings()["github_access_token"] }
+    issues_url = GITHUB_BASE_URL + repo['owner'] + "/" + repo['name'] + "/issues?state=closed&per_page=100&since=" + get_report_start_date()
+    json_data = []
+    while True:
+        try:
+            response = requests.get(issues_url, headers=headers)
+            json_data = json_data + response.json()
+            issues_url = get_next_page_url(response)
+        except Exception as e:
+            # no more pages to retrieve
+            break
+    return json_data
 
 def get_repos():
     """
     Loads repository data from repos.json into JSON object
     """
     try:
-        with open('repos.json') as data_file:    
+        with open("repos.json") as data_file:    
             repos = json.load(data_file)
         return repos
     except:
         print "Error loading repos.json"
         sys.exit()
+
+def get_next_page_url(response):
+    links = parse_link_header(response.headers["link"])
+    return links["next"]
 
 def parse_link_header(link):
     """
@@ -98,9 +94,10 @@ def write_issue_report():
     """
     print "preparing report..."
     report = open(OUTPUT_DIRECTORY + "/report-" + time.strftime("%Y-%m-%dT%H:%M:%SZ") + ".txt", 'w')
-    i = 0
-    while i < REPORT_WEEKS:
-        week_end_date = REPORT_END_DATE - timedelta(days = i * 7)
+    report_weeks = get_report_weeks()
+    report_end_date = get_report_end_date()
+    for week_number in range(0, report_weeks):
+        week_end_date = report_end_date - timedelta(days = week_number * 7)
         week_start_date = week_end_date - timedelta(days = 6)
         report_header = "Issues completed from " + week_start_date.strftime("%m/%d/%Y") + " to " + week_end_date.strftime("%m/%d/%Y")
         report.write("==============================================\n")
@@ -113,18 +110,16 @@ def write_issue_report():
             with open("data/" + data_file) as df:    
                 data = json.load(df)
 
-            for pr in data:
-                pr_closed_at = dateutil.parser.parse(pr['closed_at']).date()
-                if week_end_date >= pr_closed_at >= week_start_date:
+            for issue in data:
+                issue_closed_at = dateutil.parser.parse(issue['closed_at']).date()
+                if week_end_date >= issue_closed_at >= week_start_date:
                     if updated is False:
                         updated = True
                         report.write("--------------------------------------\n" + data_file.replace("_", "/") + ":\n--------------------------------------\n\n")
-                    line = ("* " + pr['title'] + "\n" + pr['html_url'] + "\n").encode('ascii', 'ignore').decode('ascii')
+                    line = ("* " + issue['title'] + "\n" + issue['html_url'] + "\n").encode('ascii', 'ignore').decode('ascii')
                     report.write(line)
             if updated is True: 
                 report.write("\n")
-
-        i = i + 1
 
 def cleanup_data_dir():
     """
@@ -157,7 +152,7 @@ def get_report_weeks():
         print "<report_weeks> required. Run with '--help' option for usage instructions."
         sys.exit()
 
-def get_report_start_date_as_ISO_string():
+def get_report_start_date():
     """
     Gets <report_start_date> command line argument and returns the date as an 
     ISO formatted timestamp to be passed to GitHub API when retreiving issues
@@ -170,7 +165,7 @@ def get_report_start_date_as_ISO_string():
         print "<report_start_date> required. Run with '--help' option for usage instructions."
         sys.exit()
 
-def get_report_end_date_as_datetime_obj():
+def get_report_end_date():
     """
     Gets the report_end_date and returns it as a datetime object
     """
